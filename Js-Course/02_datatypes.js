6731ffa2a7607d0f3ea944d68322e2db59461ba9
Nitin Sharma <s19nitin@gmail.com>
2024-02-08 08:00:27 +0000

    Datatypes

---
+++ Js-Course/02_datatypes.js
@@ -0,0 +1,29 @@
+
+// Datatypes in Javascript
+
+// Primitives
+
+let nm = "Naveen Manali";
+const email = "nitz19@outlook.com";
+let age = 25;
+let isLoggedIn = true;
+let address;
+let hobby = null;
+let food = "";
+
+console.log(typeof nm, typeof email, typeof age, typeof isLoggedIn, typeof address, typeof hobby, typeof food);
+
+console.log(nm, email, age, isLoggedIn, address, hobby, food);
+
+// Non - primitives
+
+let my_object = {
+    Harry : 2,
+    Larry : 4,
+    Cherry : 6,
+    Sherry : 8,
+}
+
+console.log(my_object, typeof my_object);
+
+console.log(my_object.Larry, typeof my_object.Larry);
