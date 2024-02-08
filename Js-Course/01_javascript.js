9c0379da4dacf95f19a6d3bc2d93bfa06f7f14a6
Nitin Sharma <s19nitin@gmail.com>
2024-02-08 07:47:09 +0000

    First Commit

--- Js-Course/01_javascript.js
+++ Js-Course/01_javascript.js
@@ -1,2 +1,9 @@
 
-console.log("Nitin is a good guy and a decent developer !!");
+const username = "Nitz19"
+const email = "nitz19@gmail.com"
+let address = "A-554-Z-30, Canbera-55647812, Australia"
+
+console.table([username, email, address])
+console.log(address);
+
+
