71379135303cd4975f7b1645c6e2a2864a25a641
Nitin Sharma <s19nitin@gmail.com>
2024-02-09 07:05:39 +0000

    ConversionComparisionOperations

---
+++ Js-Course/03_conversion&operations.js
@@ -0,0 +1,126 @@
+
+// let myNumber = "35";
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Number(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+// let myNumber = "35abc"
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Number(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+
+// let myNumber = null
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Number(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+// let myNumber = undefined;
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Number(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+// let myNumber = true;
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Number(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+// let myNumber = 1
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = Boolean(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+// let myNumber = 35
+
+// console.log(myNumber, typeof myNumber);
+
+// myNumber = String(myNumber)
+
+// console.log(myNumber, typeof myNumber);
+
+
+/* Operations in Javascript */
+
+
+// let str1 = "Hello";
+
+// let str2 = " naveen"
+
+// console.log(str1 + str2);
+
+// let result = "5" + 2;
+// let result = 2 + "5";
+
+
+// let result = "5" - 2;
+// let result = 2 - "5";
+
+// let result = "5" * 2;
+// let result = 2 * "5";
+
+// let result = "5" / 2;
+// let result = 2 / "5";
+
+// let result = "5" % 2;
+
+// console.log(result, typeof result);
+
+// let result = "5" + 2 + 2;
+// let result = 2 + 2 + "5";
+
+// console.log(result, typeof result);
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 4; Not a good way
+
+// num1 = 4
+// num2 = 4
+// num3 = 4
+
+// console.table([num1, num2, num3]);
+
+
+// let x = 4;
+
+// const y = x++;
+
+// console.log(`X : ${x} and Y : ${y}`);
+
+// let x = 4;
+
+// const y = ++x;
+
+// console.log(`X : ${x} and Y : ${y}`);
+
+
+/* Comparision */ // Comparision must be done in javascript through strict check like ===
+
+
+// let a = "5"
+// let b = 5
+
+// console.log(a===b);  // false
+
+// let a = 5
+// let b = 5
+
+// console.log(a===b);  // true
