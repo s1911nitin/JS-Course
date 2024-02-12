b3a96d57e52f7c877430718993c5a1c5b2aa53d0
Nitin Sharma <s19nitin@gmail.com>
2024-02-12 06:12:11 +0000

    Number

---
+++ Js-Course/06_number.js
@@ -0,0 +1,31 @@
+
+let myNum = 5025;
+
+// console.log(myNum, typeof myNum);
+
+// let myNumber = new Number(5468);
+
+// console.log(myNumber, typeof myNumber);
+// console.log(myNumber.toFixed(2), typeof myNumber.toFixed(2));
+// console.log(myNumber.toPrecision(3), typeof myNumber.toPrecision(3));
+// console.log(myNumber.toString(), typeof myNumber.toString());
+
+let myNumber = 10000000;
+// console.log(myNumber.toLocaleString("en-IN"), typeof myNumber.toLocaleString("en-IN"));
+
+
+
+/*---------------Math Library---------------*/
+
+// console.log(Math.PI);
+// console.log(Math.abs(-10));
+// console.log(Math.sqrt(25));
+// console.log(Math.cbrt(125));
+// console.log(Math.pow(2,5));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.1));
+// console.log(Math.round(5.245));
+
+/* How to fetch a random number between 10 to 20 included (10 and 20) ? */
+
+console.log(Math.floor(Math.random()*11) + 10);
