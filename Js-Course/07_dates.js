6dbd469ce774ca3ef636e7421cc7cebcad4b8227
Nitin Sharma <s19nitin@gmail.com>
2024-02-12 07:49:38 +0000

    Date Done

---
+++ Js-Course/07_dates.js
@@ -0,0 +1,40 @@
+
+let myDate = new Date();
+
+// console.log(myDate, typeof myDate);
+// console.log(myDate.toDateString(), typeof myDate.toDateString());
+// console.log(myDate.toISOString(), typeof myDate.toISOString());
+// console.log(myDate.toJSON(), typeof myDate.toJSON());
+// console.log(myDate.toLocaleDateString(), typeof myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString(), typeof myDate.toLocaleString());
+// console.log(myDate.toString(), typeof myDate.toString());
+// console.log(myDate.toTimeString(), typeof myDate.toTimeString());
+// console.log(myDate.toUTCString(), typeof myDate.toUTCString());
+
+// console.log(myDate, typeof myDate);
+
+let newDate = new Date(2024,1,12);
+// console.log(newDate, typeof newDate);
+// console.log(newDate.getDate()); // Exact Date number 
+// console.log(newDate.getDay()); // Day starts from 0 indexing for Sun.
+// console.log(newDate.getFullYear());
+// console.log(myDate.getHours());
+// console.log(myDate.getMilliseconds());
+// console.log(myDate.getMonth()); // Month starts from 0 indexing for Jan.
+// console.log(myDate.getSeconds());
+// console.log(myDate.getTime());
+
+
+let Month_Arr = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+let WeekDay_Arr = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+let month = newDate.getMonth();
+let day = newDate.getDay()
+let month_date = newDate.getDate();
+let fullYear = newDate.getFullYear();
+
+let finalDate = `Date is ${WeekDay_Arr[day]}, ${month_date}-${Month_Arr[month]}-${fullYear}`
+console.log(finalDate, typeof finalDate);
+
+
+
