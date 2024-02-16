088f7ebc53557c7a1768de9531e669d9825c0b02
Nitin Sharma <s19nitin@gmail.com>
2024-02-16 13:30:23 +0000

    Update if else ladder

--- Js-Course/13_if_else.js
+++ Js-Course/13_if_else.js
@@ -16,7 +16,7 @@
 // Write a program to give a license to a person 
 
 
-let age = 81;
+// let age = 81;
 
 // if (age < 18) {
 //     console.log("Cannot drive a car !");
@@ -33,10 +33,79 @@ let age = 81;
 //     console.log("Surely you can drive !!");
 // }
 
-if (age < 18) {
-    console.log("Cannot drive a car !");
-} else if (age == 18 || age <= 60) {
-    console.log("Can drive a car !");
-} else {
-    console.log("You must not drive a car now !!");
+// if (age < 18) {
+//     console.log("Cannot drive a car !");
+// } else if (age == 18 || age <= 60) {
+//     console.log("Can drive a car !");
+// } else {
+//     console.log("You must not drive a car now !!");
+// }
+
+// Switch Statement :
+
+
+let monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let month = 12;
+
+// switch (month) {
+//     case 0:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 1:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 2:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 3:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 4:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 5:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 6:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 0:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 7:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 8:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 9:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 10:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+//     case 11:
+//         console.log("Month: ", monthArray[month]);
+//         break;
+
+//     default:
+//         console.log("Default Output");
+//         break;
+// }
+
+
+// truty or falsy statement
+
+
+const userEmail = "nitz9@outlook.com";
+// const userEmail = "";
+// const userEmail = null;
+
+if (userEmail) {
+    console.log("Email is ", userEmail);
+}else {
+    console.log("No email");
 }
+
+
